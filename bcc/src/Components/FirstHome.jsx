72babf780cca791cb14f3a35361bde2886dafcac
Ww <nickolaswewe2@gmail.com>
2024-03-09 17:06:13 +0700

    report 09-03-24

--- bcc/src/Components/FirstHome.jsx
+++ bcc/src/Components/FirstHome.jsx
@@ -2,22 +2,26 @@ import Logo from "../images/Logo.png"
 const FirstHome = () => { 
     return(
         <>
-        <div className="bg-green-800 px-38 py-8">
-            <div className="flex pl-5">
-                <img src={Logo}></img>
-                <h1>Scofinity</h1>
-            <div className="flex">
-                <ul className="flex">
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Scholarship</li>
-                    <li>Bootcamp & Program</li>
-                    <li>Help</li>
-                    <li>Sign Up</li>
-                    <li>Log in</li>
+        <div className="font-Poppins px-38 py-8">
+            <div className="flex pl-20 space-x-1">
+                <img className="w-9 h-4 mt-1"src={Logo}></img>
+                <h1 className="font-bold text-center">Scofinity</h1>
+                <ul className="flex pl-72 space-x-7">
+                    <button><li className="">Home</li></button>
+                    <button><li>About Us</li></button>
+                    <button><li>Scholarship</li></button>
+                    <button><li>Bootcamp & Program</li></button>
+                    <button><li>Help</li></button>
                 </ul>
+            <div className=" flex pl-80 space-x-4">
+                <button className="bg-white border-spacing-1"><h1>Sign Up</h1></button>
+                <button className="bg-purple-300"><h1>Log in</h1></button>
             </div>
             </div>
+        </div>
+
+        <div>
+            Homepage
         </div>
         </>
     )
